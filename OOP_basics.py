7325915aa9a6916691b0792b5a04e046c0280706
Szymon Janiszewski <58219271+Simeno10@users.noreply.github.com>
2024-07-11 22:18:49 +0200

    Update OOP_basics.py
    car_2 added

--- OOP_basics.py
+++ OOP_basics.py
@@ -1,7 +1,9 @@
 from car import Car
 
 car_1 = Car("Porsche","911", 2021, "blue")
+car_2 = Car("Ferrari","F355", 1998, "red")
 
 print(car_1.color)
+print(car_2.year)
 car_1.drive()
-car_1.stop()
+car_2.stop()
