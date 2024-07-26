cb56f953945c97a81338f2a77da89b361f4a16f9
Szymon Janiszewski <58219271+Simeno10@users.noreply.github.com>
2024-07-26 17:57:53 +0200

    Update OOP_basics.py
    Adding Inheritance 1

--- OOP_basics.py
+++ OOP_basics.py
@@ -1,4 +1,5 @@
 from car import Car
+from car import Electric
 
 car_1 = Car("Porsche","911", 2021, "blue")
 car_2 = Car("Ferrari","F355", 1998, "red")
@@ -13,3 +14,7 @@ print(car_2.wheels)
 print(Car.wheels)
 Car.wheels = 5
 print(car_1.wheels)
+
+car2 = Electric("Nissan","Qashqai", 2012, "orange")
+print(car2.wheels)
+car2.battery()
