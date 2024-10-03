330dcabbc8e8ccd09ca7c49014350659210710ed
Szymon Janiszewski <58219271+Simeno10@users.noreply.github.com>
2024-10-03 19:16:59 +0200

    Update progressbar.py
    canvas added

--- progressbar.py
+++ progressbar.py
@@ -19,6 +19,18 @@ window = Tk()
 percent = StringVar()
 text = StringVar()
 
+canvas = Canvas(window, height=500, width=500)
+#blueLine = canvas.create_line(0,0,500,500, fill="blue", width=5)
+#redLine = canvas.create_line(0,500,500,0, fill="red", width=5)
+#canvas.create_rectangle(50,50,250,250)
+points =[250,0,500,500,0,500]
+#canvas.create_polygon(points,fill="green",outline="black", width=5)
+#canvas.create_arc(0,0,500,500, style=PIESLICE, start=270, extent=180)
+canvas.create_arc(0,0,500,500,fill="red", extent=180, width=10)
+canvas.create_arc(0,0,500,500,fill="white", start=180, extent=180, width=10)
+canvas.create_oval(190,190,310,310, fill="white", width=10)
+canvas.pack()
+
 bar = Progressbar(window, orient=HORIZONTAL, length=300)
 bar.pack(pady=10)
 
