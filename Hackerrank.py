17454aecc4eae4f786cd26fb129f2e91cd8945f1
Szymon Janiszewski <58219271+Simeno10@users.noreply.github.com>
2024-12-16 17:57:54 +0100

    Update Hackerrank.py
    Day 11: 2D Arrays

--- Hackerrank.py
+++ Hackerrank.py
@@ -9,18 +9,17 @@ import sys
 
 
 if __name__ == '__main__':
-    n = int(input().strip())
-    n = str(bin(n))
+
+    arr = []
+
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
     maxi = 0
-    count = 0
-    for i in range(len(n)):
-        if(n[i]=='1'):
-            count +=1
-        elif(maxi<count):
-            maxi = count
-            count = 0
-        else:
-            count = 0
-    if(maxi<count):
-        maxi = count
+    sumi = 0
+    
+    for i in range (len(arr)-2):
+        for j in range (len(arr)-2):
+            sumi = arr[i+1][j+1]+arr[i][j]+arr[i][j+2]+arr[i][j+1]+arr[i+2][j]+arr[i+2][j+2]+arr[i+2][j+1]
+            if(sumi > maxi or (i==0 and j==0)):
+                maxi = sumi
     print(maxi)
