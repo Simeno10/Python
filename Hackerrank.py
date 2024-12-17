3419e83add2b3d3fc2bf0c32fcf853b0ec665b16
Szymon Janiszewski <58219271+Simeno10@users.noreply.github.com>
2024-12-17 18:05:22 +0100

    Update Hackerrank.py
    Day 12: Inheritance

--- Hackerrank.py
+++ Hackerrank.py
@@ -1,25 +1,46 @@
-#!/bin/python3
+class Person:
+	def __init__(self, firstName, lastName, idNumber):
+		self.firstName = firstName
+		self.lastName = lastName
+		self.idNumber = idNumber
+	def printPerson(self):
+		print("Name:", self.lastName + ",", self.firstName)
+		print("ID:", self.idNumber)
 
-import math
-import os
-import random
-import re
-import sys
+class Student(Person):
+    def __init__(self, firstName, lastName, idNum, scores):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.idNum = idNum
+        self.scores = scores
+    def printPerson(self):
+        print("Name:", lastName+",", firstName)
+        print("ID:", idNum)
+    def calculate(self):
+        sumi = 0
+        for i in range(len(scores)):
+            sumi += scores[i]
+        score = int(sumi/len(scores))
+        if (score>=90):
+            return "O"
+        elif (score>=80 and score<90):
+            return "E"
+        elif (score>=70 and score<80):
+            return "A"
+        elif (score>=55 and score<70):
+            return "P"
+        elif (score>=40 and score<55):
+            return "D"
+        else:
+            return "T"
 
 
-
-if __name__ == '__main__':
-
-    arr = []
-
-    for _ in range(6):
-        arr.append(list(map(int, input().rstrip().split())))
-    maxi = 0
-    sumi = 0
-    
-    for i in range (len(arr)-2):
-        for j in range (len(arr)-2):
-            sumi = arr[i+1][j+1]+arr[i][j]+arr[i][j+2]+arr[i][j+1]+arr[i+2][j]+arr[i+2][j+2]+arr[i+2][j+1]
-            if(sumi > maxi or (i==0 and j==0)):
-                maxi = sumi
-    print(maxi)
+line = input().split()
+firstName = line[0]
+lastName = line[1]
+idNum = line[2]
+numScores = int(input()) # not needed for Python
+scores = list( map(int, input().split()) )
+s = Student(firstName, lastName, idNum, scores)
+s.printPerson()
+print("Grade:", s.calculate())
