4f444b7da47ffb8c5e1ee0a8963de498b3200524
Szymon Janiszewski <58219271+Simeno10@users.noreply.github.com>
2024-12-18 14:38:28 +0100

    Update Hackerrank.py
    Day 13: Abstract Classes

--- Hackerrank.py
+++ Hackerrank.py
@@ -1,46 +1,23 @@
-class Person:
-	def __init__(self, firstName, lastName, idNumber):
-		self.firstName = firstName
-		self.lastName = lastName
-		self.idNumber = idNumber
-	def printPerson(self):
-		print("Name:", self.lastName + ",", self.firstName)
-		print("ID:", self.idNumber)
+from abc import ABCMeta, abstractmethod
+class Book(object, metaclass=ABCMeta):
+    def __init__(self,title,author):
+        self.title=title
+        self.author=author   
+    @abstractmethod
+    def display(): pass
 
-class Student(Person):
-    def __init__(self, firstName, lastName, idNum, scores):
-        self.firstName = firstName
-        self.lastName = lastName
-        self.idNum = idNum
-        self.scores = scores
-    def printPerson(self):
-        print("Name:", lastName+",", firstName)
-        print("ID:", idNum)
-    def calculate(self):
-        sumi = 0
-        for i in range(len(scores)):
-            sumi += scores[i]
-        score = int(sumi/len(scores))
-        if (score>=90):
-            return "O"
-        elif (score>=80 and score<90):
-            return "E"
-        elif (score>=70 and score<80):
-            return "A"
-        elif (score>=55 and score<70):
-            return "P"
-        elif (score>=40 and score<55):
-            return "D"
-        else:
-            return "T"
+class MyBook:
+    def __init__(self, title, author, price):
+        self.title = title
+        self.author = author
+        self.price = price
+    def display(self):
+        print("Title:", self.title)
+        print("Author:", self.author)
+        print("Price:",price)
 
-
-line = input().split()
-firstName = line[0]
-lastName = line[1]
-idNum = line[2]
-numScores = int(input()) # not needed for Python
-scores = list( map(int, input().split()) )
-s = Student(firstName, lastName, idNum, scores)
-s.printPerson()
-print("Grade:", s.calculate())
+title=input()
+author=input()
+price=int(input())
+new_novel=MyBook(title,author,price)
+new_novel.display()
