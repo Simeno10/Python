d963e42f77becdce71a4d387c2b27f67bc4818f0
Szymon Janiszewski <58219271+Simeno10@users.noreply.github.com>
2024-12-19 18:04:47 +0100

    Update Hackerrank.py
    Day 14: Scope

--- Hackerrank.py
+++ Hackerrank.py
@@ -1,23 +1,14 @@
-from abc import ABCMeta, abstractmethod
-class Book(object, metaclass=ABCMeta):
-    def __init__(self,title,author):
-        self.title=title
-        self.author=author   
-    @abstractmethod
-    def display(): pass
+class Difference:
+    def __init__(self, a):
+        self.__elements = a
+    def computeDifference(self):
+        self.maximumDifference = max(self.__elements)-min(self.__elements)
+# End of Difference class
 
-class MyBook:
-    def __init__(self, title, author, price):
-        self.title = title
-        self.author = author
-        self.price = price
-    def display(self):
-        print("Title:", self.title)
-        print("Author:", self.author)
-        print("Price:",price)
+_ = input()
+a = [int(e) for e in input().split(' ')]
 
-title=input()
-author=input()
-price=int(input())
-new_novel=MyBook(title,author,price)
-new_novel.display()
+d = Difference(a)
+d.computeDifference()
+
+print(d.maximumDifference)
