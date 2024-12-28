a29913f64fbb88112ab24176a65a571648c06ecc
Szymon Janiszewski <58219271+Simeno10@users.noreply.github.com>
2024-12-28 14:03:18 +0100

    Update Hackerrank.py
    Day 15: Linked List

--- Hackerrank.py
+++ Hackerrank.py
@@ -1,14 +1,28 @@
-class Difference:
-    def __init__(self, a):
-        self.__elements = a
-    def computeDifference(self):
-        self.maximumDifference = max(self.__elements)-min(self.__elements)
-# End of Difference class
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
+class Solution: 
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
 
-_ = input()
-a = [int(e) for e in input().split(' ')]
+    def insert(self,head,data): 
+        if(head is None):
+            head = Node(data)
+        else:
+            current = head
+            while(current.next):
+                current = current.next
+            current.next = Node(data)
+        return head
 
-d = Difference(a)
-d.computeDifference()
-
-print(d.maximumDifference)
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)    
+mylist.display(head); 
