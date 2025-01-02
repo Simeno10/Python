c01a82b5cb1d3e47104a9321fcb73e01b19e25c6
Szymon Janiszewski <58219271+Simeno10@users.noreply.github.com>
2025-01-02 13:01:08 +0100

    Update PythonEssentialTraining.py
    Code challenge: Compressing ASCII art

--- PythonEssentialTraining.py
+++ PythonEssentialTraining.py
@@ -1,14 +1,33 @@
-# Python code​​​​​​‌‌​​​‌‌​​‌​​​​‌​​‌‌​‌​​‌​ below
-class NonIntArgumentException(Exception):
-    pass
+# Python code​​​​​​‌‌​​​‌‌​​‌‌​‌​​​​​​‌‌​‌‌​ below
+import json 
 
-def handleNonIntArguments(func):
-    def wrapper(*args):
-        try:
-            for i in args:
-                if(type(i) is not int):
-                    raise NonIntArgumentException("NonIntArgumentException")
-            return func(*args)
-        except TypeError:
-            print('There was a type error!')
-    return wrapper
+def encodeString(stringVal):
+    encodedList = []
+    prevChar = None
+    count = 0
+    for char in stringVal:
+        if prevChar != char and prevChar is not None:
+            encodedList.append((prevChar, count))
+            count = 0
+        prevChar = char
+        count = count + 1
+    encodedList.append((prevChar, count))
+    return encodedList
+
+def decodeString(encodedList):
+    decodedStr = ''
+    for item in encodedList:
+        decodedStr = decodedStr + item[0] * item[1]
+    return decodedStr
+
+# The filename that will be passed to this function
+# is 10_04_challenge_art.txt
+def encodeFile(filename, newFilename):
+    with open (filename) as f:
+        data = encodeString(f.read())
+    with open(newFilename, 'w') as d:
+        d.write(json.dumps(data))
+def decodeFile(filename):
+    with open(filename) as f:
+        data = f.read()
+    return decodeString(json.loads(data))
