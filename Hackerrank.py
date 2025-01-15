73d0c074774290c3e2d34537e9eb240a96928805
Szymon Janiszewski <58219271+Simeno10@users.noreply.github.com>
2025-01-15 19:27:59 +0100

    Update Hackerrank.py
    Day 18: Queues and Stacks

--- Hackerrank.py
+++ Hackerrank.py
@@ -1,21 +1,41 @@
-#Write your code here
-class Calculator:
-    def __init__(self):
-        pass
-    def power(self,n,p):
-        if(n < 0 or p < 0):
-            return "n and p should be non-negative"
-        sum = 1
-        for i in range(p):
-            sum = sum * n
-        return sum
+import sys
 
-myCalculator=Calculator()
-T=int(input())
-for i in range(T):
-    n,p = map(int, input().split())
-    try:
-        ans=myCalculator.power(n,p)
-        print(ans)
-    except Exception as e:
-        print(e)
+class Solution:
+    def __init__(self):
+        self.stack = []
+        self.queue = []
+    def pushCharacter(self, ch):
+        self.stack.append(ch)
+    def popCharacter(self):
+        return self.stack.pop()
+    def enqueueCharacter(self, ch):
+        self.queue.append(ch)
+    def dequeueCharacter(self):
+        return self.queue.pop(0)
+        
+# read the string s
+s=input()
+#Create the Solution class object
+obj=Solution()   
+
+l=len(s)
+# push/enqueue all the characters of string s to stack
+for i in range(l):
+    obj.pushCharacter(s[i])
+    obj.enqueueCharacter(s[i])
+    
+isPalindrome=True
+'''
+pop the top character from stack
+dequeue the first character from queue
+compare both the characters
+''' 
+for i in range(l // 2):
+    if obj.popCharacter()!=obj.dequeueCharacter():
+        isPalindrome=False
+        break
+#finally print whether string s is palindrome or not.
+if isPalindrome:
+    print("The word, "+s+", is a palindrome.")
+else:
+    print("The word, "+s+", is not a palindrome.") 
