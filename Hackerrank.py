cf419f3970c445651ac95a629c4e0f776268b3d2
Szymon Janiszewski <58219271+Simeno10@users.noreply.github.com>
2025-03-14 14:26:21 +0100

    Update Hackerrank.py
    Day 26: Nested Logic

--- Hackerrank.py
+++ Hackerrank.py
@@ -1,11 +1,24 @@
-def isPrime(x):
-    if x < 2:
-        return "Not prime"
-    for i in range(2, int(x**0.5) + 1):
-        if x % i == 0:
-            return "Not prime"
-    return "Prime"
-n = int(input())
-for i in range (n):
-    x = int(input())
-    print(isPrime(x))
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import sys
+
+def calculate_fine(returned_date, due_date):
+    ret_day, ret_month, ret_year = map(int, returned_date)
+    due_day, due_month, due_year = map(int, due_date)
+    
+    if (ret_year, ret_month, ret_day) <= (due_year, due_month, due_day):
+        return 0
+    elif ret_year > due_year:
+        return 10000
+    elif ret_month > due_month and ret_year == due_year:
+        return 500 * (ret_month - due_month)
+    else:
+        return 15 * (ret_day - due_day)
+
+if __name__ == "__main__":
+    lines = sys.stdin.read().strip().split("\n")
+    
+    returned_date = lines[0].split()
+    due_date = lines[1].split()
+    
+    fine = calculate_fine(returned_date, due_date)
+    print(fine)
