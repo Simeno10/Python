abc1babe237007715233a023981547676275a9c8
Szymon Janiszewski <58219271+Simeno10@users.noreply.github.com>
2025-03-21 17:30:18 +0100

    Update Hackerrank.py
    Day 29: Bitwise AND

--- Hackerrank.py
+++ Hackerrank.py
@@ -6,17 +6,38 @@ import random
 import re
 import sys
 
+#
+# Complete the 'bitwiseAnd' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER N
+#  2. INTEGER K
+#
 
+def bitwiseAnd(N, K):
+    max_and = 0
+    for A in range(1, N):
+        for B in range(A + 1, N + 1):
+            and_value = A & B
+            if max_and < and_value < K:
+                max_and = and_value
+    return max_and
 
 if __name__ == '__main__':
-    N = int(input().strip())
-    gmail_users = []
-    
-    for _ in range(N):
-        firstName, emailID = input().rstrip().split()
-        
-        if emailID.endswith("@gmail.com"):
-            gmail_users.append(firstName)
-    
-    for name in sorted(gmail_users):
-        print(name)
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        count = int(first_multiple_input[0])
+
+        lim = int(first_multiple_input[1])
+
+        res = bitwiseAnd(count, lim)
+
+        fptr.write(str(res) + '\n')
+
+    fptr.close()
