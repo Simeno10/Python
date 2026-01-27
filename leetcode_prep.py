d3edf827300914eaaa74c98b210c38f707a3e3ae
Szymon Janiszewski <58219271+Simeno10@users.noreply.github.com>
2026-01-27 19:57:57 +0100

    Update leetcode_prep.py
    Binary Search

--- leetcode_prep.py
+++ leetcode_prep.py
@@ -1,19 +1,13 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution(object):
-    def mergeTwoLists(self, list1, list2):
-        
-        cur = dummy = ListNode()
-        while list1 and list2:
-            if list1.val < list2.val:
-                cur.next = list1
-                list1, cur = list1.next, list1
+    def search(self, nums, target):
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = (left + right) / 2
+            if nums[mid] == target:
+                return mid
+            elif target > nums[mid]:
+                left = mid + 1
             else:
-                cur.next = list2
-                list2, cur = list2.next, list2
-        if list1 or list2:
-            cur.next = list1 if list1 else list2
-        return dummy.next 
+                right = mid - 1
+        return -1
